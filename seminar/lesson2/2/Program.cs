a09f2ccee0de24fa52b4b418e2a846de66d12a7f
yuri26nev <109887823+yuri26nev@users.noreply.github.com>
2022-08-10 15:21:50 +0300

    Создан репозиторий для домашних заданий

---
+++ seminar/lesson2/2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// 
+// интересная штука tabnine, воспользуюсь им для закрепления англиского и поробую писать коменты на англиском.
+
+Console.Clear(); // Clear screen
+Console.WriteLine("Введите число, и я поробую угадать третью цифру этого число");     // print string "input namber" 
+string namberText = Convert.ToString(Console.ReadLine());  // input namber in console and convert to string tape
+// check number for hundredths
+if (namberText.Length > 2)
+{
+    Console.WriteLine("Введенном вами числе " + namberText + " ,третья цифра это -> " + namberText[2]);
+}
+else
+{
+    Console.WriteLine("Введенном вами числе " + namberText + " ,третьей цифры нет ");
+}
+
---
+++ seminar/lesson2/3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+// является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Clear(); // Clear the console 
+Console.WriteLine("Введите цифру  обозначающую день недели от 1 до 7 и я покажу является этот день выходным ");
+int namber = Convert.ToInt32(Console.ReadLine());
+// create an array with the day
+string[] day = {"Понедельнику", "Вторнику", "Среде", "Четвергу", "Пятнице", "Субботе", "Воскресенью"};
+// namber check for conditions 
+if (namber > 0 && namber < 8)
+{
+    if (namber == 6 || namber == 7)
+    {
+        Console.WriteLine("Веденное вами число " + namber + " соответствует " + day[namber-1] + ". Это выходной");
+    }
+    else Console.WriteLine(" Веденное вами число  " + namber + " соответствует " + day[namber-1] + ". Это рабочий день");
+}
+else
+{
+    Console.WriteLine("Веденное вами число " + namber + " не соответствует условию ввода.");
+}
