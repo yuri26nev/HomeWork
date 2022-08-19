5587f58d00544bacbf62124fa35d9813bcc55f71
yuri26nev <109887823+yuri26nev@users.noreply.github.com>
2022-08-19 07:53:32 +0300

    й

--- seminar/lesson4/Program.cs
+++ seminar/lesson4/Program.cs
@@ -11,11 +11,12 @@
 // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
 Console.Clear();
 int extent (int A,int B){
     int rezalty = A;
     for (int i = 1; i < B ; i++)
-    {        rezalty = rezalty*A;
+    {        rezalty = rezalty*A;й
     Console.WriteLine(i+"="+rezalty);
     }
 return rezalty;
